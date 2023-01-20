27d8f5eab85b3b6fb874e0385f1a2585c181bb4b
Dennis Renz <dennis.renz@relaxdays.de>
2023-01-20 16:00:08 +0100

    Setup project structure

---
+++ TestUtilities.Scenarios.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace TestUtilities.Scenarios.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ TestUtilities.Scenarios/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace TestUtilities.Scenarios;
+public class Class1
+{
+
+}
